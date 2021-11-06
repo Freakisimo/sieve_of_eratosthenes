0cf256f199a0bdd159660ae7222fb47938a56465
dante7 <dante.arkade7@gmail.com>
2021-11-05 19:21:46 -0600

    Improve calculation using asyn functions, chunked list and recursion over head list for filter the composive numbers

--- lib/prime_opt.ex
+++ lib/prime_opt.ex
@@ -4,20 +4,41 @@ defmodule PrimeOpt do
   """
 
   def main(input) do
+    
+    chunk_size = :math.sqrt(input) |> Float.ceil(0) |> trunc
+
     2..input
+    |> Enum.to_list
+    |> Enum.chunk_every(chunk_size)
     |> calculate_primes
+    |> length
+
   end
 
-  def calculate_primes(serie) do
-    recursive_prime(Enum.to_list(serie), [])
+  def calculate_primes([head | tail]) do
+
+    primes = recursive_prime(head, [])
+    another_primes = 
+    for l <- tail do
+      Task.async(fn -> remove_multiples(primes, l) end)
+    end
+    |> Task.yield_many(10_000)
+    |> Enum.map(fn {t, res} -> elem(res, 1) || Task.shutdown(t, :brutal_kill) end)
+    |> Enum.concat
+    primes ++ another_primes
+
   end
 
   def recursive_prime([head | tail], primes) do
-    rem = Enum.filter(tail, fn x -> rem(x, head) != 0 end)
-    # IO.puts length(rem)
-    recursive_prime(rem, primes ++ [head])
+    recursive_prime(Enum.filter(tail, fn x -> rem(x, head) != 0 end), primes ++ [head])
   end
 
   def recursive_prime([], list_primes), do: list_primes
+
+  def remove_multiples([head | tail], number_list) do
+    remove_multiples(tail, Enum.filter(number_list, fn x -> rem(x, head) != 0 end))
+  end
+
+  def remove_multiples([], number_list), do: number_list
 
 end
